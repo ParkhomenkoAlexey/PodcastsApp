cb41ea16aeaab31c4c4ca15a5eed649511f3c037
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2019-07-08 15:12:49 +0500

    3 урок

--- PodcastsApp/MainTabBarController.swift
+++ PodcastsApp/MainTabBarController.swift
@@ -20,16 +20,16 @@ class MainTabBarController: UITabBarController {
         tabBar.tintColor = .purple
 
         viewControllers = [
+            generateNavigationController(with: PodcastsSearchController(), title: "Search", image: #imageLiteral(resourceName: "search")),
             generateNavigationController(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
-            generateNavigationController(with: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
             generateNavigationController(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
         ]
     }
     
     func generateNavigationController(with rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
         let navController = UINavigationController(rootViewController: rootViewController)
-        rootViewController.navigationItem.title = title
 //        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
         navController.tabBarItem.title = title
         navController.tabBarItem.image = image
         return navController
---
+++ PodcastsApp/Models/PodcastResponse.swift
@@ -0,0 +1,16 @@
+//
+//  PodcastResponse.swift
+//  PodcastsApp
+//
+//  Created by Алексей Пархоменко on 07/07/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import Foundation
+
+struct Podcast {
+    let name: String
+    let artistName: String
+}
+
+
